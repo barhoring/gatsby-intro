e7225efee0383db843b7be69fdb0d48889211427
Bar Horing Amir <bar.apple@gmail.com>
2020-11-03 12:19:36 +0200

    Creating & Styling The Header

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Header from '../components/header';
 
 const Layout = ({ children }) => (
   <>
@@ -55,7 +56,7 @@ const Layout = ({ children }) => (
         }
       `}
     />
-    <header></header>
+    <Header />
     <main
       css={css`
         margin: 2rem auto 4rem;
