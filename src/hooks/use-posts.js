68d5a0d57efa4f2c7944491c0bfb56436ae0be3e
Bar Horing Amir <bar.apple@gmail.com>
2020-11-07 13:15:15 +0200

    Rendering a Post Listing

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,26 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    author: post.frontmatter.author,
+    slug: post.frontmatter.slug,
+    excerpt: post.excerpt,
+  }));
+};
+
+export default usePosts;
